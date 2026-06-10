ef161649ec5c1997f3044806dcc55c3d1f57cf37
Paras Tripathi <ptparas172@gmail.com>
2026-06-10 11:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (65.15%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char,int> val;
-        for(char i: s){
-            val[i]++;
-        }
-        int res = 0;
-        bool odd = false;
-        for(auto it : val){
-            if(it.second%2==0){
-                res=res+it.second;
-            }
-            else {
-                res=res+it.second-1;
-                odd = true;
-            }
-        }
-        if(odd==true){
-            return res+1;
-        } else return res;
-    }
+        unordered_map<char,int> mp;
+        int ans = 0;
 
+        for(char c : s) mp[c]++;
+
+        for(auto it : mp) {
+            ans += (it.second / 2) * 2;
+            if(ans % 2 == 0 && it.second % 2 == 1)
+                ans++;
+        }
+
+        return ans;
+    }
 };
