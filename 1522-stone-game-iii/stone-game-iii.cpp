f0f9ddaecbfc5b012ddfb7aa9d7df047f077e213
Paras Tripathi <ptparas172@gmail.com>
2026-08-03 20:37:18 +0530

    Time: 28 ms (61.87%) | Memory: 141.5 MB (46.66%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> dp;
+    int n;
+
+    int solve(int i, vector<int>& stoneValue) {
+        if (i >= n)
+            return 0;
+
+        if (dp[i] != INT_MIN)
+            return dp[i];
+
+        int take = 0;
+        int ans = INT_MIN;
+
+        for (int k = 0; k < 3 && i + k < n; k++) {
+            take += stoneValue[i + k];
+            ans = max(ans, take - solve(i + k + 1, stoneValue));
+        }
+
+        return dp[i] = ans;
+    }
+
+    string stoneGameIII(vector<int>& stoneValue) {
+        n = stoneValue.size();
+        dp.assign(n, INT_MIN);
+
+        int diff = solve(0, stoneValue);
+
+        if (diff > 0)
+            return "Alice";
+        else if (diff < 0)
+            return "Bob";
+        else
+            return "Tie";
+    }
+};
