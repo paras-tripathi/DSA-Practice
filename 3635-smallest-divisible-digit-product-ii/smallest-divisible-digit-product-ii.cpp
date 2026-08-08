8e94769b149a8b658b268bf4195b3e658e624406
Paras Tripathi <ptparas172@gmail.com>
2026-08-08 12:01:55 +0530

    Time: 35 ms (70.00%) | Memory: 37.3 MB (57.33%) - LeetSync

--- 3635-smallest-divisible-digit-product-ii/smallest-divisible-digit-product-ii.cpp
+++ 3635-smallest-divisible-digit-product-ii/smallest-divisible-digit-product-ii.cpp
@@ -1,133 +1,227 @@
 class Solution {
 public:
+
+    // factor[d] = number of prime factors {2,3,5,7}
+    vector<vector<int>> factor = {
+        {0, 0, 0, 0}, // 0
+        {0, 0, 0, 0}, // 1
+        {1, 0, 0, 0}, // 2
+        {0, 1, 0, 0}, // 3
+        {2, 0, 0, 0}, // 4
+        {0, 0, 1, 0}, // 5
+        {1, 1, 0, 0}, // 6
+        {0, 0, 0, 1}, // 7
+        {3, 0, 0, 0}, // 8
+        {0, 2, 0, 0}  // 9
+    };
+
+    // Convert required prime factors into digits
+    vector<int> getDigits(vector<int> cnt) {
+
+        vector<int> digits(10, 0);
+
+        // 2^3 -> 8
+        digits[8] = cnt[0] / 3;
+        cnt[0] %= 3;
+
+        // 3^2 -> 9
+        digits[9] = cnt[1] / 2;
+        cnt[1] %= 2;
+
+        // Remaining 2^2 -> 4
+        digits[4] = cnt[0] / 2;
+        cnt[0] %= 2;
+
+        // Remaining 2
+        digits[2] = cnt[0];
+
+        // Remaining 3
+        digits[3] = cnt[1];
+
+        /*
+            Important optimization:
+
+            2 + 3 -> 6
+
+            4 + 3 -> 2 + 6
+
+            This makes the resulting number lexicographically smaller.
+        */
+
+        if (digits[2] == 1 && digits[3] == 1) {
+            digits[2] = 0;
+            digits[3] = 0;
+            digits[6] = 1;
+        }
+
+        if (digits[3] == 1 && digits[4] == 1) {
+            digits[3] = 0;
+            digits[4] = 0;
+            digits[2] = 1;
+            digits[6] = 1;
+        }
+
+        digits[5] = cnt[2];
+        digits[7] = cnt[3];
+
+        return digits;
+    }
+
+    int countDigits(vector<int>& digits) {
+        int res = 0;
+
+        for (int d = 1; d <= 9; d++)
+            res += digits[d];
+
+        return res;
+    }
+
+    string construct(vector<int>& digits) {
+
+        string ans;
+
+        // Increasing order gives smallest number
+        for (int d = 2; d <= 9; d++) {
+            ans += string(digits[d], char('0' + d));
+        }
+
+        return ans;
+    }
+
     string smallestNumber(string num, long long t) {
-        int req2 = 0, req3 = 0, req5 = 0, req7 = 0;
-        long long temp = t;
-        while (temp % 2 == 0) { temp /= 2; req2++; }
-        while (temp % 3 == 0) { temp /= 3; req3++; }
-        while (temp % 5 == 0) { temp /= 5; req5++; }
-        while (temp % 7 == 0) { temp /= 7; req7++; }
-        if (temp > 1) return "-1";
 
-        int dp[60][40];
-        for (int i = 0; i < 60; ++i) {
-            for (int j = 0; j < 40; ++j) {
-                dp[i][j] = 1e9;
-            }
-        }
-        dp[0][0] = 0;
-        
-        int trans[6][2] = {{1, 0}, {0, 1}, {2, 0}, {1, 1}, {3, 0}, {0, 2}};
-        for (int i = 0; i < 60; ++i) {
-            for (int j = 0; j < 40; ++j) {
-                if (dp[i][j] == 1e9) continue;
-                for (auto& tr : trans) {
-                    int ni = min(59, i + tr[0]);
-                    int nj = min(39, j + tr[1]);
-                    dp[ni][nj] = min(dp[ni][nj], dp[i][j] + 1);
-                }
-            }
-        }
-        for (int i = 59; i >= 0; --i) {
-            for (int j = 39; j >= 0; --j) {
-                if (i < 59) dp[i][j] = min(dp[i][j], dp[i + 1][j]);
-                if (j < 39) dp[i][j] = min(dp[i][j], dp[i][j + 1]);
+        // Required prime factors of t
+        vector<int> need(4, 0);
+
+        int primes[] = {2, 3, 5, 7};
+
+        for (int i = 0; i < 4; i++) {
+
+            while (t % primes[i] == 0) {
+                need[i]++;
+                t /= primes[i];
             }
         }
 
-        int F2[] = {0, 0, 1, 0, 2, 0, 1, 0, 3, 0};
-        int F3[] = {0, 0, 0, 1, 0, 0, 1, 0, 0, 2};
-        int F5[] = {0, 0, 0, 0, 0, 1, 0, 0, 0, 0};
-        int F7[] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0};
+        // t contains a prime factor other than 2,3,5,7
+        if (t != 1)
+            return "-1";
 
-        int n = num.length();
-        bool has_zero = false;
-        int first_zero = n;
-        for (int i = 0; i < n; ++i) {
-            if (num[i] == '0') {
-                has_zero = true;
-                first_zero = i;
-                break;
-            }
+        // Minimum digits needed
+        vector<int> minDigits = getDigits(need);
+
+        int minLen = countDigits(minDigits);
+        int n = num.size();
+
+        // If even the minimum representation is longer,
+        // it is the answer.
+        if (minLen > n) {
+            return construct(minDigits);
         }
 
-        if (!has_zero) {
-            int r2 = req2, r3 = req3, r5 = req5, r7 = req7;
-            for (char c : num) {
-                int d = c - '0';
-                r2 = max(0, r2 - F2[d]);
-                r3 = max(0, r3 - F3[d]);
-                r5 = max(0, r5 - F5[d]);
-                r7 = max(0, r7 - F7[d]);
-            }
-            if (r2 == 0 && r3 == 0 && r5 == 0 && r7 == 0) return num;
-        }
+        // Count factors in num
+        vector<int> prefix(4, 0);
 
-        int limit = min(n - 1, first_zero);
-        int p2 = 0, p3 = 0, p5 = 0, p7 = 0;
-        for (int i = 0; i < limit; ++i) {
+        int firstZero = n;
+
+        for (int i = 0; i < n; i++) {
+
             int d = num[i] - '0';
-            p2 += F2[d];
-            p3 += F3[d];
-            p5 += F5[d];
-            p7 += F7[d];
+
+            if (d == 0 && firstZero == n)
+                firstZero = i;
+
+            for (int j = 0; j < 4; j++)
+                prefix[j] += factor[d][j];
         }
 
-        for (int i = limit; i >= 0; --i) {
-            int start_d = (num[i] - '0') + 1;
-            for (int d = start_d; d <= 9; ++d) {
-                int n2 = max(0, req2 - p2 - F2[d]);
-                int n3 = max(0, req3 - p3 - F3[d]);
-                int n5 = max(0, req5 - p5 - F5[d]);
-                int n7 = max(0, req7 - p7 - F7[d]);
-                int L = n - 1 - i;
-                
-                if (n7 + n5 + dp[n2][n3] <= L) {
-                    string ans = num.substr(0, i) + to_string(d);
-                    int rem2 = n2, rem3 = n3, rem5 = n5, rem7 = n7;
-                    for (int pos = 0; pos < L; ++pos) {
-                        for (int x = 1; x <= 9; ++x) {
-                            int nn2 = max(0, rem2 - F2[x]);
-                            int nn3 = max(0, rem3 - F3[x]);
-                            int nn5 = max(0, rem5 - F5[x]);
-                            int nn7 = max(0, rem7 - F7[x]);
-                            if (nn7 + nn5 + dp[nn2][nn3] <= L - 1 - pos) {
-                                ans += to_string(x);
-                                rem2 = nn2; rem3 = nn3; rem5 = nn5; rem7 = nn7;
-                                break;
-                            }
-                        }
-                    }
-                    return ans;
-                }
-            }
-            if (i > 0) {
-                int d = num[i - 1] - '0';
-                p2 -= F2[d];
-                p3 -= F3[d];
-                p5 -= F5[d];
-                p7 -= F7[d];
-            }
-        }
+        // num itself is valid
+        if (firstZero == n) {
 
-        int min_len_needed = req7 + req5 + dp[req2][req3];
-        int M = max(n + 1, min_len_needed);
-        string ans = "";
-        int rem2 = req2, rem3 = req3, rem5 = req5, rem7 = req7;
-        
-        for (int pos = 0; pos < M; ++pos) {
-            for (int x = 1; x <= 9; ++x) {
-                int nn2 = max(0, rem2 - F2[x]);
-                int nn3 = max(0, rem3 - F3[x]);
-                int nn5 = max(0, rem5 - F5[x]);
-                int nn7 = max(0, rem7 - F7[x]);
-                if (nn7 + nn5 + dp[nn2][nn3] <= M - 1 - pos) {
-                    ans += to_string(x);
-                    rem2 = nn2; rem3 = nn3; rem5 = nn5; rem7 = nn7;
+            bool ok = true;
+
+            for (int j = 0; j < 4; j++) {
+                if (prefix[j] < need[j]) {
+                    ok = false;
                     break;
                 }
             }
+
+            if (ok)
+                return num;
         }
-        return ans;
+
+        /*
+            Try changing one digit.
+
+            Start from the right because we want to keep
+            the longest possible prefix unchanged.
+        */
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int current = num[i] - '0';
+
+            // Remove current digit from prefix
+            for (int j = 0; j < 4; j++)
+                prefix[j] -= factor[current][j];
+
+            // If this is after the first zero, prefix would
+            // already contain zero, so it is invalid.
+            if (i > firstZero)
+                continue;
+
+            // Try the smallest larger digit
+            for (int d = current + 1; d <= 9; d++) {
+
+                vector<int> remaining(4);
+
+                for (int j = 0; j < 4; j++) {
+
+                    int available =
+                        prefix[j] + factor[d][j];
+
+                    remaining[j] =
+                        max(0, need[j] - available);
+                }
+
+                vector<int> suffixDigits =
+                    getDigits(remaining);
+
+                int suffixLen =
+                    countDigits(suffixDigits);
+
+                int space =
+                    n - i - 1;
+
+                if (suffixLen <= space) {
+
+                    string ans = num.substr(0, i);
+
+                    ans += char('0' + d);
+
+                    // Unused positions can be 1
+                    ans += string(
+                        space - suffixLen,
+                        '1'
+                    );
+
+                    ans += construct(suffixDigits);
+
+                    return ans;
+                }
+            }
+        }
+
+        /*
+            No valid answer with the same length.
+            Construct the smallest valid number with
+            one additional digit.
+        */
+
+        return string(
+            n + 1 - minLen,
+            '1'
+        ) + construct(minDigits);
     }
 };
