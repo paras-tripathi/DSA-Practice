0b1f6ab6a9416777110ef65b6c532e6a32facf63
Paras Tripathi <ptparas172@gmail.com>
2026-09-25 11:42:45 +0530

    Time: 3 ms (79.78%) | Memory: 19.7 MB (13.66%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.py
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -10,14 +10,12 @@ class Solution:
                 else:
                     break
             return res, i
-
         def parseTerm(i):
             res = {""}
             while i < len(expression) and expression[i] != '}' and expression[i] != ',':
                 factor, i = parseFactor(i)
                 res = {a + b for a in res for b in factor}
             return res, i
-
         def parseFactor(i):
             if expression[i] == '{':
                 i += 1 
@@ -30,6 +28,5 @@ class Solution:
                     j += 1
                 word = expression[i:j]
                 return {word}, j
-
         ans, _ = parseExpr(0)
         return sorted(list(ans))
